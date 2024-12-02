acd30bc0a14a6bb851df8da39fd66e991f571988
shivam-mishra-26 <114924867+shivam-mishra-26@users.noreply.github.com>
2024-12-02 20:41:15 +0530

    Update backend.tf
    add bucket name

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "my-ews-baket1122"
+    bucket         = "my-ews-baket11223"
     region         = "us-east-2"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "Lock-Files"
